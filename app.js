ceb2c659ce314840dc72fa6c0c8c18ffacba884d
Dennis McDonald <dennis@cambridgecomputing.us>
2024-04-02 11:11:07 -0500

    Converted to MongoDB

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 // Main app.
 require('dotenv').config()
-const sql = require('./sql')
-const line = '-'.repeat(process.stdout.columns)
-
-// Collect environment variables
-if(typeof process.env.SQL_SERVER === 'undefined'){
-    console.log("Error: No SQL server address defined! Make sure that SQL_SERVER environment veriable is set.")
-    process.exit(1)
-}
-if(typeof process.env.SQL_USER === 'undefined'){
-    console.log("Error: No SQL user defined! Make sure that SQL_USER environment veriable is set.")
-    process.exit(1)
-}
-if(typeof process.env.SQL_PASSWORD === 'undefined'){
-    console.log("Error: No SQL user password defined! Make sure that SQL_PASSWORD environment veriable is set.")
-    process.exit(1)
-}
-if(typeof process.env.SQL_DATABASE === 'undefined'){
-    console.log("Error: No SQL database name defined! Make sure that SQL_DATABASE environment veriable is set.")
-    process.exit(1)
-}
+const dataCollection = require('./data-collection')
 
 function main(cb) {
-
+    dataCollection.readLogs()
 }
 
 console.log("Starting main loop.")
-setInterval(() => {
-    main(()=>{
-        console.log("Loop complete")
-    })
-}, (28800000))
+// setInterval(() => {
+//     main(()=>{
+//         console.log("Loop complete")
+//     })
+// }, (28800000))
+
+// Run every hour
+const mainLoop = setInterval(main, 60 * 60 * 1000)
--- data-collection.js
+++ data-collection.js
@@ -1,43 +1,97 @@
 const fs = require('fs')
 const readline = require('readline')
 const db = require('./db')
+const smb2 = require('smb2');
+
 let lineNumber = 0
 let headers = []
 let objEntry = {}
 
-// Initialize the database if this is the first run.
-db.initDB()
+const dhcpServer		= process.env.DHCP_SERVER
+const dhcpLogsDir  		= process.env.DHCP_LOGS_DIR
+const dhcpUser          = process.env.DHCP_USER
+const dhcpPassword      = process.env.DHCP_PASSWORD
+const shareName         = "C$\\" + dhcpLogsDir
 
-// Collect data from remote servers
-const fileStream = fs.createReadStream('./DhcpSrvLog-Thu.log')   //Static file for testing
+async function downloadFile(destinationPath) {
+    const client = new smb2({ 
+        share: `\\\\${dhcpServer}\\\\${shareName}`,
+        domain: 'NTSERVER', // Usually WORKGROUP or your pc name
+        username: dhcpUser, 
+        password: dhcpPassword
+    })
+    
+    const filePath = filePathOnServer.replace(/\\/g, "/") // Replace windows path separator with UNIX one
+    const remoteFile = client.util.fullRemotePath(filePath)
 
-// Read most recent DHCP log file
-const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-});
+    try {
+        await client.connect();
+        
+        let data = [];
 
-// Ingest CSV contents. Om nom.
-// Ignore initial lines that just have documentation included (33 lines)
-rl.on('line', (entry) => {
-    lineNumber++
-    // Read each entry into an array "data" that can be correlated with the "headers" array
-    if (lineNumber > 34){
-    // if (lineNumber == 72){  //TESTING
-        let data = entry.split(',')
-        i = 0
-        headers.forEach(header => {
-            objEntry[header] = data[i]
-            i++
-        })
-        // Submit to database if the entry contains a MAC Address
-        if (objEntry.MACAddress !== ""){
-            console.log(lineNumber + ": Adding entry " + objEntry.MACAddress + " to DB.")
-            db.storeData(objEntry,() =>{})
-        }
-    } else if (lineNumber == 34){
-        // Read the CSV header, remove all whitespaces
-        entry = entry.replace(/\s+/g, '')
-        headers = entry.split(',')
+        // Read the file from server in chunks
+        const readStream = await client.readFile(remoteFile, {start: 0});
+
+        readStream.on('data', chunk => {
+            data.push(chunk);
+        });
+
+        readStream.on('end', async () => {
+            // Concatenate the chunks into a Buffer and write it to disk
+            await fs.promises.writeFile(destinationPath, Buffer.concat(data));
+            
+            console.log(`File downloaded successfully at ${destinationPath}`);
+        });
+        
+        readStream.on('error', error => {
+            throw new Error("Error while downloading file: " + error.message);
+        });
+    } catch (error) {
+        console.error("Error while connecting to server: ", error);
+    } finally {
+        client.destroy(); // Close the connection
     }
-});
+}
+
+// Fetch and parse DHCP server logs from a remote Windows server.
+exports.readLogs = function(){
+
+
+
+
+
+
+
+    // TODO: Fetch from SMB share
+    const fileStream = fs.createReadStream('./DhcpSrvLog-Thu.log')   //Static file for testing
+
+    // Read most recent DHCP log file
+    const rl = readline.createInterface({
+        input: fileStream
+    })
+
+    // Ingest CSV contents. Om nom.
+    // Ignore initial lines that are just documentation (first 33 lines)
+    rl.on('line', (entry) => {
+        lineNumber++
+        // Read the headers into an array that will be used to set the object's property names
+        if (lineNumber == 34){
+            // Read the CSV header, remove all whitespaces
+            entry = entry.replace(/\s+/g, '')
+            headers = entry.split(',')
+        } else if (lineNumber > 34){
+        // } else if (lineNumber == 72){  //TESTING
+            let data = entry.split(',')
+            i = 0
+            headers.forEach(header => {
+                objEntry[header] = data[i]
+                i++
+            })
+            // Submit to database if the entry contains a MAC Address
+            if (objEntry.MACAddress !== ""){
+                console.log(lineNumber + ": Adding entry " + objEntry.MACAddress + " to DB.")
+                db.storeData(objEntry)
+            }
+        }
+    })
+}
